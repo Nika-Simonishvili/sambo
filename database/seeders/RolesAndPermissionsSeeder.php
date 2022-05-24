b664cb9d899399a0d73391ef4490f1375abc58f7
NikaS <n.simonishvili@sweeftdigital.com>
2022-05-24 16:39:22 +0400

    test for frontend

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    private $permissions = [
+        'manage referee',
+        'manage coach',
+        'manage athlete',
+        'manage tournament'
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        foreach ($this->permissions as $permission) {
+            Permission::updateOrCreate(['name' => $permission]);
+        }
+
+        // create roles and assign created permissions
+        $role = Role::create(['name' => 'admin'])
+            ->givePermissionTo(Permission::all());
+
+        $role = Role::create(['name' => 'coach'])
+            ->givePermissionTo(['manage athlete', 'manage coach']);
+    }
+}
+
